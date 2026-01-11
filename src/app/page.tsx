218f8b4192f11ed5cd96f517afd176ff8a768810
AYAZALAM19 <ayazalamjakirali@gmail.com>
2026-01-11 20:29:05 +0530

    fix the Best option error in build third time

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
 import { type FC } from "react";
 import CoffeeSliderBanner from "../components/CoffeeSliderBanner";
 import Newsletter from "../components/Newsletter";
-import BestOptions from "@/components/landing/BestOptions";
-import Stats from "@/components/landing/Stats";
-import PremiumCollection from "@/components/landing/PremiumCollection";
-import AboutCaffie from "@/components/landing/AboutCaffie";
-import Qualities from "@/components/landing/Qualities";
-import AboutProduct from "@/components/landing/AboutProduct";
+import BestOptions from "../components/landing/BestOptions";
+import Stats from "../components/landing/Stats";
+import PremiumCollection from "../components/landing/PremiumCollection";
+import AboutCaffie from "../components/landing/AboutCaffie";
+import Qualities from "../components/landing/Qualities";
+import AboutProduct from "../components/landing/AboutProduct";
 
 export default function HomePage() {
   return (
